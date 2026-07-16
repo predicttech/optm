52e00742713d1d3f94afdd9e796e38c4364ee5b3
aliimam-in <imam30114@gmail.com>
2026-07-16 14:31:45 +0530

    world

--- components/home/strip.tsx
+++ components/home/strip.tsx
@@ -5,7 +5,7 @@ export function StripSection() {
     return (
         <section className="z-50 px-6 relative w-full gap-2 flex justify-center items-center py-2">
             <p className="text-sm text-center font-medium max-w-60 md:max-w-full">
-                India's first biomarker-driven pain & metabolic clinic. 
+                World's first biomarker-driven pain & metabolic clinic. 
             </p> 
         </section> 
     );
